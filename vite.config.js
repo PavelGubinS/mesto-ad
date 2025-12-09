f29b473903248ed092ee76c7149d3469fe8dad73
PavelGubinS <gubin357@mail.ru>
2025-12-09 20:30:44 +0500

    Билд проекта и правки для Git Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
     open: true,
   },
+
+  build: {
+    outDir: "dist",
+    emptyOutDir: true, // чистка dist перед каждой сборкой
+  },
+
+  base: "/mesto-ad/",
 });
